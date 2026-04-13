eebf03776c58df9153fe69955317b7301c86c7d7
Dev-Danish-Ac <imddanish19@gmail.com>
2026-04-13 13:14:44 +0530

    sln

---
+++ stitchinpilot_sln/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StitchinPilot_Devp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Main API!");
+        }
+    }
+}
---
+++ stitchinpilot_sln/Controllers/sampleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StitchinPilot_Devp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class sampleController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Second Controller");
+        }
+    }
+}
